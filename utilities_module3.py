e1d3dd29d506d76159ba7d94320d185620527677
Kewalin Samart <57643647+KewalinSamart@users.noreply.github.com>
2022-11-06 20:36:29 -0700

    add read_in_solutions func

--- utilities_module3.py
+++ utilities_module3.py
@@ -1,4 +1,24 @@
-# by default, this program would generat random prix fixe solutions using the gmt file for FA 
+import pandas as pd 
+
+def read_in_solutions(chosen_genes_filename, sep='\t'):
+    # solutions: 1       2       3
+    #            gene3   gene5   gene1
+    #            gene4   gene2   gene3
+    #            .       .       .
+    chosen_genes_df = pd.read_table(chosen_genes_filename, delimiter=sep)
+    chosen_genes = list(chosen_genes_df.itertuples(index=False, name=None))
+    loci_candidate_dict = {}
+    annotated_candidate_dict = {}
+    for locus in chosen_genes_df.columns:
+        loci_candidate_dict[locus] = list(set(chosen_genes_df[locus]))
+    annotated_candidate_dict = {}
+    for key, val in loci_candidate_dict.items():
+        for gene in val:
+            annotated_candidate_dict[gene] = key
+
+    return chosen_genes, loci_candidate_dict, annotated_candidate_dict
+
+# by default, this program would generate random prix fixe solutions using the gmt file for FA 
 def get_loci_candidate_genes(loci_genes_filename = "toy_loci_set.txt"):
     '''
     This function read in txt file with loci and their candidate genes
@@ -15,12 +35,15 @@ def get_loci_candidate_genes(loci_genes_filename = "toy_loci_set.txt"):
             candidate_genes = line.split("\t")[1:]
             candidate_genes = [elem.replace('Locus for ', '') for elem in candidate_genes]
             candidate_genes = [elem.replace('\n', '') for elem in candidate_genes]
-            #candidate_genes = [elem.split(", ") for elem in candidate_genes]
         else:
             candidate_genes = line.split("\t")[1:]
         # remove empty strings ("")
         candidate_genes = list(filter(None, candidate_genes))
         loci_candidate_dict[loci_index] =  candidate_genes
         loci_index = loci_index + 1
-    return loci_candidate_dict
 
+        annotated_candidate_dict = {}
+        for key, val in loci_candidate_dict.items():
+            for gene in val:
+                annotated_candidate_dict[gene] = key
+    return loci_candidate_dict,  annotated_candidate_dict
