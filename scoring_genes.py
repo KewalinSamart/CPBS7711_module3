ffb772165743ab89a693cf4ec5112db1bf2fd524
Kewalin Samart <57643647+KewalinSamart@users.noreply.github.com>
2022-11-06 20:38:53 -0700

    get final sol func

--- scoring_genes.py
+++ scoring_genes.py
@@ -1,4 +1,6 @@
 from network import *
+from PF_solutions import *
+from utilities_module3 import * 
 import numpy as np
 
 def gene_substitution(locus_index, itr_gene, solutions, sol_index):
@@ -41,3 +43,51 @@ def score_gene(locus_index, chosen_replaced, network):
     gene_score = np.abs(density - empty_locus_density)
 
     return gene_score
+
+def get_final_solution(solutions_filename = None, network_filename = None, flip_weight = 'no', output_dir=None): 
+    # by default
+    if solutions_filename == None:
+        loci_candidate_dict,  annotated_candidate_dict = get_loci_candidate_genes()
+        # create Prix fixe solutions object
+        solutions = PFsolutions(loci_candidate_dict, annotated_candidate_dict)
+        # randomly generate chosen genes for each solution (one gene per locus)
+        solutions.generate_chosen_genes()
+    # solutions given
+    else:
+         # create Prix fixe solutions object
+        chosen_genes, loci_candidate_dict, annotated_candidate_dict = read_in_solutions(chosen_genes_filename = solutions_filename)
+        solutions = PFsolutions(loci_candidate_dict, annotated_candidate_dict, chosen_genes)
+    
+     # create network object
+    if network_filename == None:
+        network = Network("STRING_network.txt")
+    else:
+        network = Network(network_filename, flip=flip_weight)
+    # call method to generate dicts storing gene-gene interactions and degrees
+    network.get_network_interactions_degrees()    
+
+    # get number of solutions = number of all possible combinations of chosen genes
+    num_sols = len(solutions.chosen_genes)
+    print("Number of input/example solutions: ",num_sols)
+    # get the generated chosen genes
+    chosen_genes = solutions.chosen_genes
+    # iterate through one solution at a time 
+    for sol_index in range(1,num_sols):
+        # iterate through one locus at a time 
+        for locus in solutions.loci_set.keys():
+            locus_gene_candidates = solutions.loci_set[locus] 
+            locus_chosen_gene = solutions.get_sol_chosen_genes(sol_index)
+            locus_itr_genes = list(set(locus_gene_candidates) - set(locus_chosen_gene))
+            # calculate score for each candidate gene
+            for itr_gene in locus_itr_genes:
+                chosen_replaced = gene_substitution(locus, itr_gene, solutions, sol_index)
+                gene_score = score_gene(locus, chosen_replaced, network)
+                solutions.update_gene_scores(itr_gene, gene_score)
+    # average scores across every solution to get final gene scores
+    solutions.compute_final_scores()
+    # finalize the result dataframe -- add loci annotation
+    solutions.finalize_final_sol()
+    # output the final solution dataframe
+    if output_dir==None:
+        output_dir='example_result/final_solution.txt'
+    solutions.output_final_sol(output_dir)
